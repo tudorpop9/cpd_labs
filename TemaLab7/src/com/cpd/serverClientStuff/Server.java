987bdc356d69ed23714233382fc1864d258a22d8
Tudor Pop <tudor_fb@yahoo.com>
2021-04-15 23:13:17 +0300

    Implemented method requests, but did not test it

--- TemaLab7/src/com/cpd/serverClientStuff/Server.java
+++ TemaLab7/src/com/cpd/serverClientStuff/Server.java
@@ -1,20 +1,25 @@
 package com.cpd.serverClientStuff;
 
+import com.cpd.entity.AirbnbClone;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.ParseException;
 
 public class Server {
     public static String IP = "127.0.0.1";
     public static int PORT = 7777;
+    public static AirbnbClone airbnbClone = new AirbnbClone();
     private ServerSocket serverSocket;
 
     public void start() throws IOException {
         serverSocket = new ServerSocket(PORT);
 
         for(;;){
-            Socket receiver = serverSocket.accept();
-            // handle client
+            Socket client = serverSocket.accept();
+
+            new ServerRequestHandler(client, airbnbClone).start();
         }
     }
 
--- TemaLab7/src/com/cpd/serverClientStuff/ServerRequestHandler.java
+++ TemaLab7/src/com/cpd/serverClientStuff/ServerRequestHandler.java
@@ -1,5 +1,7 @@
 package com.cpd.serverClientStuff;
 
+import com.cpd.entity.AirbnbClone;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,9 +12,11 @@ public class ServerRequestHandler extends Thread{
     private Socket client;
     private BufferedReader receiver;
     private PrintWriter sender;
+    private AirbnbClone airbnbClone;
 
-    public ServerRequestHandler(Socket client) {
+    public ServerRequestHandler(Socket client, AirbnbClone airbnbClone) {
         this.client = client;
+        this.airbnbClone = airbnbClone;
     }
 
     @Override
@@ -21,16 +25,23 @@ public class ServerRequestHandler extends Thread{
             receiver = new BufferedReader(new InputStreamReader(client.getInputStream()));
             sender = new PrintWriter(client.getOutputStream(), true);
 
-            String reuqest;
-            while ((reuqest = receiver.readLine()) != null ) {
-                if("disconnect".equals(reuqest)) {
+            String request;
+            while ((request = receiver.readLine()) != null ) {
+                if("disconnect".equals(request)) {
                     sender.println("you are disconnected");
                     break;
                 }
 
+                try{
+                    this.performRequest(request);
+                }catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+                    sender.println("Not enough parameters, or wrong format");
+                }catch (NumberFormatException numberFormatException){
+                    sender.println("invalid number format, or parameter order");
+                }catch (Exception e){
+                    sender.println("Unsupported operation / Unknown exception ");
+                }
 
-
-//                sender.println("received: " + reuqest);
             }
 
             client.close();
@@ -38,6 +49,45 @@ public class ServerRequestHandler extends Thread{
             sender.close();
         }catch (IOException e){
             e.printStackTrace();
+        }
+    }
+
+    private void performRequest(String request) throws Exception {
+        String[] splitReq = request.split(" ", 2);
+        String reqName = splitReq[0];
+
+        switch (reqName){
+            case "addUser":
+                String[] addUserParams = splitReq[1].split(" ");
+                String userName = addUserParams[0];
+                String userRole = addUserParams[1];
+
+                this.airbnbClone.addUser(userName, userRole);
+                break;
+            case "addPlace":
+                String[] requestParams = splitReq[1].split(" ");
+                String city = requestParams[0];
+                String placeName = requestParams[1];
+                long ownerId = Long.parseLong(requestParams[2]);
+
+                this.airbnbClone.addPlace(city, placeName, ownerId);
+                break;
+            case "schedulePlace":
+                String[] schedulePlaceParams = splitReq[1].split(" ");
+                String citySc = schedulePlaceParams[0];
+                String placeNameSc = schedulePlaceParams[1];
+                String monthSc = schedulePlaceParams[2];
+                int yearSc = Integer.parseInt(schedulePlaceParams[3]);
+                long touristIdSc = Long.parseLong(schedulePlaceParams[4]);
+
+                this.airbnbClone.schedulePlace(citySc, placeNameSc, monthSc, yearSc, touristIdSc);
+                break;
+            case "viewAllAvailableMonths":
+
+                this.airbnbClone.viewAllAvailableMonths();
+                break;
+            default:
+                throw new Exception("Unsupported operation");
         }
     }
 }
